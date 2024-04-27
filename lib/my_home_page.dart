a7e0df1f12f1765c1f70a963d3538135d144a07c
Muhammad Aditya Nugraha <117628774+AdityaNugraha1@users.noreply.github.com>
2024-04-28 01:23:19 +0700

    Update my_home_page.dart

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+List<Map<String, String>> favoriteSites = [];
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -351,7 +353,12 @@ class _AreaCalculatorState extends State<AreaCalculator> {
   }
 }
 
-class SitusRekomendasiPage extends StatelessWidget {
+class SitusRekomendasiPage extends StatefulWidget {
+  @override
+  _SitusRekomendasiPageState createState() => _SitusRekomendasiPageState();
+}
+
+class _SitusRekomendasiPageState extends State<SitusRekomendasiPage> {
   final List<Map<String, String>> situsRekomendasi = [
     {"nama": "9GAG", "url": "https://9gag.com/"},
     {"nama": "xkcd", "url": "https://xkcd.com/"},
@@ -375,6 +382,17 @@ class SitusRekomendasiPage extends StatelessWidget {
     {"nama": "WikiHow", "url": "https://www.wikihow.com/"},
   ];
 
+  void toggleFavorite(Map<String, String> site) {
+    setState(() {
+      final isFavorite = favoriteSites.any((item) => item["url"] == site["url"]);
+      if (isFavorite) {
+        favoriteSites.removeWhere((item) => item["url"] == site["url"]);
+      } else {
+        favoriteSites.add(site);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -384,16 +402,25 @@ class SitusRekomendasiPage extends StatelessWidget {
       body: ListView.builder(
         itemCount: situsRekomendasi.length,
         itemBuilder: (BuildContext context, int index) {
+          final site = situsRekomendasi[index];
+          final isFavorite = favoriteSites.any((item) => item["url"] == site["url"]);
           return ListTile(
-            title: Text(situsRekomendasi[index]["nama"]!),
-            subtitle: Text(situsRekomendasi[index]["url"]!),
+            title: Text(site["nama"]!),
+            subtitle: Text(site["url"]!),
+            trailing: IconButton(
+              icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: isFavorite ? Colors.red : null,
+              onPressed: () {
+                toggleFavorite(site);
+              },
+            ),
             onTap: () async {
-              final url = situsRekomendasi[index]["url"]!;
+              final url = site["url"]!;
               if (await canLaunch(url)) {
                 await launch(url);
               } else {
                 ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Tidak dapat membuka ${situsRekomendasi[index]["nama"]}')),
+                  SnackBar(content: Text('Could not launch $url')),
                 );
               }
             },
@@ -411,8 +438,16 @@ class FavoritePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Favorite'),
       ),
-      body: Center(
-        child: Text('Halaman Favorite'),
+      body: ListView.builder(
+        itemCount: favoriteSites.length,
+        itemBuilder: (context, index) {
+          final site = favoriteSites[index];
+          return ListTile(
+            title: Text(site["nama"]!),
+            subtitle: Text(site["url"]!),
+            trailing: Icon(Icons.favorite, color: Colors.red),
+          );
+        },
       ),
     );
   }
